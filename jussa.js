e39c6a8c79dea67eeb55b4b9f7137e41a8d3e0f9
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-05-21 11:37:35 +0500

    14th commit

--- jussa.js
+++ jussa.js
@@ -4,6 +4,4 @@ window.addEventListener("scroll", () => {
   let value = window.scrollY;
   //   console.log(value);
   headerText.style.opacity = "100" - value * 1 + "%";
-  headerText.style.position = "reletive";
-  headerText.style.top = value * 3 + "px";
 });
